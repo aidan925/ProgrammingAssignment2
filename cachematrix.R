c007adde84a9c784d3333b91f7ac06e3450eb89a
aidan925 <39339922+aidan925@users.noreply.github.com>
2018-07-12 10:49:46 -0700

    Matrix Caching Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take a non-singular matrix as input and return
+## its inverse by solving if the input is new, or returning a cached 
+## value is the input has been solved before
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a list of functions to be used in cacheSolve
+makeCacheMatrix <- function(x = matrix()){
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list( set = set, get = get, setinverse = setinverse,
+              getinverse = getinverse)
 }
 
+## This fucntion returns the cached value of the input if present
+## or solves the input if no cached value is present
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x, ...){
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        data <- x[get()]
+        i <- solve(data, ...)
+        x[setinverse(i)]
+        i
+}        
+     
+    
+        
+        
